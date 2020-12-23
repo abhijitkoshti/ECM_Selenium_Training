d6682ca7422aa2d492a5cd9a3c7d7c543b92a874
abhijitkoshti <abhijit.koshti@gmail.com>
2020-12-23 12:22:34 +0100

    Added a new file

---
+++ src/test/java/com/OrangeHrm/DragDrop2.java
@@ -0,0 +1,17 @@
+package com.OrangeHrm;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DragDrop2 {
+ 
+	
+	public void DragDrop() throws InterruptedException{
+		WebDriverManager.chromedriver().version("86.0.4240.22").setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+	}
+	
+}
